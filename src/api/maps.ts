9405285e883dc42f8ac39b902c406680a3e52ea2
Jorge Neira <jorgengarriga@gmail.com>
2021-09-30 21:23:24 +0200

    feat: add initialize google map script

---
+++ src/api/maps.ts
@@ -0,0 +1,39 @@
+import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+  apiKey: "AIzaSyAydqbK7-DVCsxufYg_FtdigjBmX5XZhTw",
+  version: "weekly",
+});
+
+const getCurrentPosition = (map: google.maps.Map) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        map.setCenter(pos);
+        map.setZoom(15);
+      }
+    );
+  }
+};
+
+export const loadMap = async (
+  mapContainer: HTMLDivElement,
+  position: Position
+) => {
+  loader
+    .load()
+    .then((google) => {
+      const map = new google.maps.Map(mapContainer as HTMLElement, {
+        center: position,
+        zoom: 13,
+      });
+      getCurrentPosition(map);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
