88f33aeb8008c02bade2584b6de13d0196c30ae9
Jorge Neira <jorgengarriga@gmail.com>
2021-09-30 21:22:27 +0200

    refactor: rename files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../Common/Map/Map";
+
+function App() {
+  return (
+    <div>
+      <Map position={{ lat: 41.390205, lng: 2.154007 }} />
+    </div>
+  );
+}
+
+export default App;
